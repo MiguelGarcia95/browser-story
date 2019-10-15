a5e3b4ab0654f569e3d9fbe28792edf1bc40c7d5
MiguelGarcia95 <mgarcia95951@gmail.com>
2019-10-15 11:31:08 -0500

    setup express

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path')
+const app = express();
+
+// configure mongoose to connect to mongodb
+mongoose.connect(process.env.MONGODB_URL, {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+}).then(res => {
+  console.log("MongoDB connected succesfully")
+}).catch(err => {
+  console.log("MongoDB connection failed.")
+});
+
+// Serve static assets 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+
+  app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server is up on port: ${process.env.PORT}`);
+}) 
