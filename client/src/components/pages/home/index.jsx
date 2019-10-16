34f6526b4e0bc0959ca677d438a56a958416deda
MiguelGarcia95 <mgarcia95951@gmail.com>
2019-10-16 00:03:38 -0500

    add test setup

--- client/src/components/pages/home/index.jsx
+++ client/src/components/pages/home/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import Navbar from '../navbar';
+import Navbar from '../../navbar';
 
 const Container = styled.div`
   height: 100vh;
