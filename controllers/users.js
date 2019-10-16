104ac7965579bd89f846892ad130f04a814ff868
MiguelGarcia95 <mgarcia95951@gmail.com>
2019-10-16 13:52:27 -0500

    create routes controller and models

---
+++ controllers/users.js
@@ -0,0 +1,19 @@
+const User = require('../models/User');
+const bcrypt = require('bcryptjs');
+
+module.exports = {
+  register: async (req, res) => {
+
+  },
+
+  login: async (req, res) => {
+
+  },
+  loginWithToken: async (req, res) => {
+
+  },  
+  logout: async (req, res) => {
+
+  },
+  
+}
---
+++ routes/users.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+const users = require('../controllers/users');
+
+router.post('/', users.register);
+router.post('/login', users.login);
+router.post('/loginWithToken', users.loginWithToken);
+router.post('/logout', users.logout);
