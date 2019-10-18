32f3efd91a943748b2e60cc24950074071720e35
MiguelGarcia95 <mgarcia95951@gmail.com>
2019-10-18 18:57:40 -0500

    add option

--- routes/options.js
+++ routes/options.js
@@ -2,9 +2,8 @@ const express = require('express');
 const router = express.Router();
 const options = require('../controllers/options');
 
-router.route('/').post(options.create);
+router.route('/').post(options.addOption);
 router.route('/:id').get(options.getOption);
-router.route('/:id/addOption').post(options.addOption);
 router.route('/edit/:id').patch(options.edit);
 //   .patch(storyMaps.updateCurrentOption);
 module.exports = router;  
