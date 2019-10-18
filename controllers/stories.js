2198e95b4af05c8408824bfda96d0d5d68d2d487
MiguelGarcia95 <mgarcia95951@gmail.com>
2019-10-17 22:00:40 -0500

    start story

--- controllers/stories.js
+++ controllers/stories.js
@@ -9,16 +9,15 @@ module.exports = {
   startStory: async (req, res) => {
     try {
       const story = await Story.findById(req.params.id);
-
       const userStoryTracker = await new StoryTracker({
         story: story._id,
         user: req.body.user._id,
       });
       await  userStoryTracker.save();
-
       res.status(201).send({userStoryTracker});
     } catch (error) {
-      
+      console.log(error);
+      res.status(400).send({error}); 
     }
   },
 
