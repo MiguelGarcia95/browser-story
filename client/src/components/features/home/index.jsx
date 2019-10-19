ae1752d56e0ee6c08072941232a2ddfba82d4aff
MiguelGarcia95 <mgarcia95951@gmail.com>
2019-10-19 18:53:03 -0500

    create components

--- client/src/components/features/home/index.jsx
+++ client/src/components/features/home/index.jsx
@@ -2,18 +2,47 @@ import React from 'react';
 import styled from 'styled-components';
 import Navbar from '../../navbar';
 
-const Container = styled.div`
+const Body = styled.div`
   height: 100vh;
   max-height: 100%;
   min-width: 100vw;
+  position: relative;
+`;
+
+const Container = styled.div`
+  width: 100vw;
+  margin: auto;
+  background: red;
+  min-height: 100vh;
+  box-sizing: border-box;
+  padding-left: 300px;
+  padding-right: 50px;
+  padding-top: 50px;
+  position: absolute;
+  left: 0;
+  top: 0;
+  z-index: 1;
+`;
+
+const Sidebar = styled.div`
+width: 250px;
+background: green;
+height: 100vh;
+padding-top: 50px;
+position: absolute;
+box-sizing: border-box;
+top: 0; left: 0;
+z-index: 2;
 `;
 
 export default function Home() {
   return (
-    <Container>
-      <Navbar name={'test'}>
-      </Navbar>
-      
-    </Container>
+    <Body>
+      <Navbar name={'test'} />
+      <Sidebar />
+      <Container>
+
+      </Container>
+    </Body>
   )
 }
--- client/src/components/navbar/index.jsx
+++ client/src/components/navbar/index.jsx
@@ -3,9 +3,14 @@ import styled from 'styled-components';
 
 const Container = styled.div`
   width: 100%;
-  height: 70px;
-  background-color: red;
+  height: 50px;
+  background-color: #fff;
+  z-index: 3;
+  position: sticky;
+  position: fixed;
 `;
+
+// f9f9f9
 
 export default function Navbar({name}) {
   return (
