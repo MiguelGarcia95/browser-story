689277ac0a89dba5a5447f34e8bd470636a222fc
MiguelGarcia95 <mgarcia95951@gmail.com>
2019-10-20 10:53:59 -0500

    connect app with redux

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {HashRouter, Switch, Route, withRouter} from 'react-router-dom';
+import {Provider, connect} from 'react-redux';
 import '@atlaskit/css-reset';
 import './index.css';
-
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 
+const Root = () => {
+  return (
+    <HashRouter>
+      <Switch>
+        <Route exact path='/' component={App} />
+      </Switch>
+    </HashRouter>
+  )
+}
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const RootWithAuth = withRouter(connect()(Root));
+
+const RootWithRouter = () => {
+  return(
+    <HashRouter>
+      <RootWithAuth />
+    </HashRouter>
+  )
+}
+
+ReactDOM.render(<RootWithRouter />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
