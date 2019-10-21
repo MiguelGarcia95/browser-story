cb0ac9232aea7ad8088e6d9aeea70ecb0108a8b1
MiguelGarcia95 <mgarcia95951@gmail.com>
2019-10-20 21:32:14 -0500

    create auth pages

--- client/src/components/auth/signup/index.jsx
+++ client/src/components/auth/signup/index.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
+import Navbar from '../../navbar';
+import Form from '../../form';
 
 const Body = styled.div`
   height: 100vh;
@@ -8,10 +10,36 @@ const Body = styled.div`
   position: relative;
 `;
 
-export default function SignUp() {
-  return (
-    <Body>
-      'signup'
-    </Body>
-  )
+const Container = styled.div`
+  width: 100vw;
+  margin: auto;
+  min-height: 100vh;
+  box-sizing: border-box;
+  padding-top: 50px;
+  z-index: 1;
+`;
+
+class SignUp extends React.Component {
+  onSubmit = (body) => {
+    console.log(body)
+  }
+
+  render() {
+    const formInputs = [
+      {name: 'username', type: 'text', placeholder: 'Enter Username'},
+      {name: 'email', type: 'email', placeholder: 'Enter Email'},
+      {name: 'password', type: 'password', placeholder: 'Enter Password'},
+      {name: 'confirm_password', type: 'password', placeholder: 'Confirm Password'},
+    ];
+    return (
+      <Body>
+        <Navbar name={''} />
+        <Container>
+          <Form inputs={formInputs} submit='Sign Up' title='Sign Up' onSubmit={this.onSubmit} />
+        </Container>
+      </Body>
+    )
+  }
 }
+
+export default SignUp;
