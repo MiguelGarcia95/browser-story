8212c63950bd1f2e7515bdd6c92467a5e19f5917
MiguelGarcia95 <mgarcia95951@gmail.com>
2019-10-26 20:50:56 -0500

    style chat component

--- client/src/components/featureComponents/chat/index.jsx
+++ client/src/components/featureComponents/chat/index.jsx
@@ -1,4 +1,18 @@
-import React from 'react'
+import React from 'react';
+
+const sampleData = {
+  messages: [
+    {from: 'id95516168', avatar: 'image.jpg', message: 'How are you man?'},
+    {from: 'you', avatar: 'image.jpg', message: 'Good. What was up with you last night?'},
+    {from: 'id95516168', avatar: 'image.jpg', message: 'What do you mean?'},
+    {from: 'you', avatar: 'image.jpg', message: 'Last night you were being weird. Always looking around as if searching for someone. We honestly thought you were on something.'},
+    {from: 'id95516168', avatar: 'image.jpg', message: 'What I meant is that we did not meet last night.'},
+    {from: 'you', avatar: 'image.jpg', message: 'Bull! Check out Sandra\'s feed. She uploaded a video from last night. You are on it.'},
+    {from: 'you', avatar: 'image.jpg', message: 'Here!', attachment: 'image.jpg'},
+    {from: 'you', avatar: 'image.jpg', message: 'At 3:41! See! There you.', attachment: 'image.jpg'},
+    {from: 'id95516168', avatar: 'image.jpg', message: 'Dude. Last night I was with my girlfriend. Check her account. She was here!'},
+  ]
+}
 
  class Chat extends React.Component {
    render() {
