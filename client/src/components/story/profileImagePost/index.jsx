4ccbea02786bb3253a3099b40f0406a2ade3a807
MiguelGarcia95 <mgarcia95951@gmail.com>
2019-11-02 22:11:32 -0500

    create profile & profile post components

--- client/src/components/story/profileImagePost/index.jsx
+++ client/src/components/story/profileImagePost/index.jsx
@@ -1,8 +1,19 @@
 import React from 'react';
 
+import styled from 'styled-components';
+
 import {Body, Container, Content} from './style';
 import Navbar from '../../featureComponents/navbar';
 import Chat from '../../featureComponents/chat';
+
+const ProfileSidebar = styled.div``;
+const ProfileHeader = styled.div``;
+const ProfileFeed = styled.div``;
+const Post = styled.div``;
+const Image = styled.div``;
+const Description = styled.div``;
+const Meta = styled.div``;
+const Coments = styled.div``;
 
 class ProfileImagePost extends React.Component {
 
