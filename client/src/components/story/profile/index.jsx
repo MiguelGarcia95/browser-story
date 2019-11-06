8e3bd8d91dbd6b255e371477c2a5e1fe18cb0f0b
MiguelGarcia95 <mgarcia95951@gmail.com>
2019-11-06 02:20:58 -0600

    style post components

--- client/src/components/story/profile/index.jsx
+++ client/src/components/story/profile/index.jsx
@@ -98,10 +98,6 @@ const ProfilePost = styled.div`
   background: blue;
 `;
 
-const PostUser = styled.div`
-
-`;
-
 const PostDescription = styled.div`
   padding: 10px;
   margin-bottom: 10px;
@@ -109,17 +105,28 @@ const PostDescription = styled.div`
   color: white;
 `;
 
+const PostUser = styled.div`
+  width: 100%;
+  height: 50px;
+`;
+
 const PostAvatar = styled.img`
   width: 25px; 
   height: 25px;
-  margin: auto;
+  margin: 12.5px;
+  float: left;
   border-radius: 50%;
   box-shadow: 0px 0px 5px 0px rgba(0,0,0,0.1);
 `;
 
 const PostUsername = styled.h2`
+  float: left;
+  line-height: 50px;
+  margin-top: 0;
   color: white;
   font-size: 1rem;
+  font-weight: 400;
+  letter-spacing: 1px;
 `;
 
 const PostImage = styled.img`
