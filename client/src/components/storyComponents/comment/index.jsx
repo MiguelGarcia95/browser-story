bf33f71ffc4e0815c6ef526410d8c0e68fecfa29
MiguelGarcia95 <mgarcia95951@gmail.com>
2019-11-07 13:39:58 -0600

    style post components

---
+++ client/src/components/storyComponents/comment/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Body = styled.div`
+
+`;
+
+function Comment() {
+
+  return (
+    <Body>
+
+    </Body>
+  )
+}
+
+export default Comment;
