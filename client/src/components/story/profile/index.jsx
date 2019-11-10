60766c71c638c0991378fa4dd450f66b57ad92e4
MiguelGarcia95 <mgarcia95951@gmail.com>
2019-11-10 01:11:57 -0600

    style post components

--- client/src/components/story/profile/index.jsx
+++ client/src/components/story/profile/index.jsx
@@ -168,11 +168,18 @@ const PostComents = styled.div`
 
 const LikeList = styled.div`
   width: 150px;
-  height: 200px;
+  height: 120px;
   background: white;
   position: absolute;
   margin-top: 35px;
   z-index: 1;
+  
+  p {
+    margin: 0 auto;
+    line-height: 20px;
+    text-align: center;
+    font-size: 0.7rem;
+  }
 `;
 
 class Home extends React.Component {
@@ -242,7 +249,12 @@ class Home extends React.Component {
 
                     {likesModal && 
                       <LikeList>
-                        List
+                        <p>Person 1</p>
+                        <p>Person 2</p>
+                        <p>Person 3</p>
+                        <p>Person 4</p>
+                        <p>Person 5</p>
+                        <p>Person 51</p>
                       </LikeList>
                     }
 
