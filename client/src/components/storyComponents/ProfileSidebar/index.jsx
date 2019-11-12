2c39a05cbbc9b7106325fdbe068e3ba264cdc4dc
MiguelGarcia95 <mgarcia95951@gmail.com>
2019-11-12 10:19:25 -0600

    style post components

---
+++ client/src/components/storyComponents/ProfileSidebar/index.jsx
@@ -0,0 +1,107 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Body = styled.div`
+  width: 350px;
+  min-height: 100vh;
+  top: 0;
+  box-sizing: border-box;
+  padding-top: 50px;
+  box-shadow: 0px 5px 5px 0px rgba(0,0,0,0.1);
+  position: absolute;
+  z-index: 2;
+  background: #f6f6f6;
+`;
+
+const ProfileHeader = styled.div`
+  width: 100%;
+  height: 200px;
+  position: relative;
+`;
+
+const HeaderAvatar = styled.img`
+  width: 100px; 
+  height: 100px;
+  top: 0; bottom: 50px;
+  left: 0; right: 0;
+  margin: auto;
+  border-radius: 50%;
+  position: absolute;
+  box-shadow: 0px 0px 5px 0px rgba(0,0,0,0.1);
+`;
+
+const HeaderUser = styled.h1`
+  height: 60px;
+  line-height: 60px;
+  font-size: 1rem;
+  width: 100%;
+  box-sizing: border-box;
+  text-align: center;
+  color: black;
+  position: absolute;
+  bottom: 0;
+`;
+
+const ProfileTitle = styled.p`
+  width: 100%;
+  box-sizing: border-box;
+  padding-left: 100px;
+  line-height: 20px;
+  padding-top: 20px;
+  padding-bottom: 20px;
+  font-size: 0.9rem;
+  color: gray;
+  border-top: 1px solid rgba(0,0,0,0.1);
+`;
+
+const ProfileDescription = styled.p`
+  width: 100%;
+  margin-top:0;
+  padding-bottom: 20px;
+  box-sizing: border-box;
+  padding-left: 100px;
+  padding-right: 100px;
+  min-height: 40px;
+  font-size: 0.9rem;
+`;
+
+const ProfileInfo = styled.div`
+  width: 100%;
+  margin-bottom: 50px;
+  // margin-top: 50px;
+  background: white;
+`;
+
+function ProfileSidebar({user}) {
+  return (
+    <Body>
+      <ProfileHeader>
+        <HeaderUser>{user.username}</HeaderUser>
+        <HeaderAvatar src={user.avatar} />
+      </ProfileHeader>
+
+      <ProfileInfo>
+        <ProfileTitle>About</ProfileTitle>
+        <ProfileDescription>Amateur photographer & designer</ProfileDescription>
+        <ProfileDescription>Member since November 2011</ProfileDescription>
+      </ProfileInfo>
+
+      <ProfileInfo>
+        <ProfileTitle>Place</ProfileTitle>
+        <ProfileDescription>Lives in Houston</ProfileDescription>
+      </ProfileInfo>
+
+      <ProfileInfo>
+        <ProfileTitle>Friends</ProfileTitle>
+        <ProfileDescription>182 Friends</ProfileDescription>
+      </ProfileInfo>
+
+      <ProfileInfo>
+        <ProfileTitle>Followers</ProfileTitle>
+        <ProfileDescription>867 Followers</ProfileDescription>
+      </ProfileInfo>
+    </Body>
+  )
+}
+
+export default ProfileSidebar;
