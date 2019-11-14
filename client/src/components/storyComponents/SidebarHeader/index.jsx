574874f6603848a10505b4bc2e26cd6c2b7844dd
MiguelGarcia95 <mgarcia95951@gmail.com>
2019-11-14 13:51:46 -0600

    style post components

---
+++ client/src/components/storyComponents/SidebarHeader/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Body = styled.div`
+`;
+
+function SidebarHeader() {
+  return (
+    <Body>
+    </Body>
+  )
+}
+
+export default SidebarHeader;
