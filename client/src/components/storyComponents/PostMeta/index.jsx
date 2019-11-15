386a789ae3a80cad6a1e36704a1d79597c001e27
MiguelGarcia95 <mgarcia95951@gmail.com>
2019-11-15 01:40:53 -0600

    style post components

--- client/src/components/storyComponents/PostMeta/index.jsx
+++ client/src/components/storyComponents/PostMeta/index.jsx
@@ -1,26 +1,6 @@
 import React from 'react';
-import styled from 'styled-components';
 import { IoIosHeart, IoIosText} from 'react-icons/io';
-
-const Body = styled.div`
-  width: 150px;
-  float: left;
-  cursor: pointer;
-  height: 30px;
-  svg {
-    float: left;
-    height: 30px;
-    margin-left: 10px;
-    margin-right: 10px;
-  }
-`;
-
-const PostMetaText = styled.p`
-  font-size: 0.9rem;
-  margin-top: 0;
-  float: left;
-  line-height: 30px;
-`;
+import { Body, PostMetaText } from "./style";
 
 function PostMeta({type, text, onClick}) {
   return (
---
+++ client/src/components/storyComponents/PostMeta/style.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+export const Body = styled.div`
+  width: 150px;
+  float: left;
+  cursor: pointer;
+  height: 30px;
+  svg {
+    float: left;
+    height: 30px;
+    margin-left: 10px;
+    margin-right: 10px;
+  }
+`;
+
+export const PostMetaText = styled.p`
+  font-size: 0.9rem;
+  margin-top: 0;
+  float: left;
+  line-height: 30px;
+`;
