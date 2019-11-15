b9539a486291a29dc06a2ee311c54acd7a41488c
MiguelGarcia95 <mgarcia95951@gmail.com>
2019-11-15 01:35:48 -0600

    style post components

--- client/src/components/storyComponents/PostMeta/index.jsx
+++ client/src/components/storyComponents/PostMeta/index.jsx
@@ -1,12 +1,32 @@
 import React from 'react';
 import styled from 'styled-components';
+import { IoIosHeart, IoIosText} from 'react-icons/io';
 
 const Body = styled.div`
+  width: 150px;
+  float: left;
+  cursor: pointer;
+  height: 30px;
+  svg {
+    float: left;
+    height: 30px;
+    margin-left: 10px;
+    margin-right: 10px;
+  }
 `;
 
-function PostMeta() {
+const PostMetaText = styled.p`
+  font-size: 0.9rem;
+  margin-top: 0;
+  float: left;
+  line-height: 30px;
+`;
+
+function PostMeta({type}) {
   return (
     <Body>
+      {type === 'like' ? <IoIosHeart size='1.1rem' /> : <IoIosText size='1.1rem' /> }
+      <PostMetaText>51 {51 > 1 ? 'Likes' : 'Like'}</PostMetaText>
     </Body>
   )
 }
